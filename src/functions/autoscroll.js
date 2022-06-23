71d1d0c2af539c3b89b597c5b5ebdcf0d6aa57bc
elmaester <olegmorket@gmail.com>
2022-06-23 12:25:30 +0300

    page autoscroll function

---
+++ src/functions/autoscroll.js
@@ -0,0 +1,23 @@
+async function autoScroll(page, step) {
+  try {
+    await page.evaluate(async (distance) => {
+      await new Promise((resolve, reject) => {
+        var totalHeight = 0;
+        var timer = setInterval(() => {
+          var scrollHeight = document.body.scrollHeight;
+          window.scrollBy(0, distance);
+          totalHeight += distance;
+
+          if (totalHeight >= scrollHeight) {
+            clearInterval(timer);
+            resolve();
+          }
+        }, 100);
+      });
+    }, step);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+module.exports = autoScroll;
