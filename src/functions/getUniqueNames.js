299d890951c6ab765dead2486eb66ab18887b507
elmaester <olegmorket@gmail.com>
2022-06-23 17:13:38 +0300

    flesh out the name parsing logic

--- src/functions/getUniqueNames.js
+++ src/functions/getUniqueNames.js
@@ -1,7 +1,28 @@
-function getUniqueNames(nodelist) {
-  const allNames = Array.from(nodelist).map(node => node.innerText);
-  const names = [...new Set(nodelist)];
-  return names;
+const selectors = require("../selectors");
+
+async function getUniqueNames(page) {
+  try {
+    return await page.evaluate((selectors) => {
+      const usernameNodes1 = document.querySelectorAll(selectors.username1);
+      const usernameNodes2 = document.querySelectorAll(selectors.username2);
+      const usernames1 = Array.from(usernameNodes1).map(
+        (node) => node.innerText
+      );
+      const usernames2 = Array.from(usernameNodes2).map(
+        (node) => node.innerText
+      );
+      const names1and2 = usernames1.concat(usernames2);
+      const allNames = names1and2
+        .map((name) => name.trim())
+        .filter(
+          (name) => name.length && name !== "singers" && name !== "singer"
+        );
+      const names = [...new Set(allNames)];
+      return names;
+    }, selectors);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 module.exports = getUniqueNames;
