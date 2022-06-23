45699ac7b347cb51048dbd32ef70cc10ed565d6d
elmaester <olegmorket@gmail.com>
2022-06-23 20:13:13 +0300

    vastly improve autoScroll logic

--- src/functions/autoScroll.js
+++ src/functions/autoScroll.js
@@ -1,20 +1,37 @@
-async function autoScroll(page, step) {
+async function autoScroll(
+  page,
+  step = 1000,
+  interval = 50,
+  idlePassesToConfirm = 50
+) {
   try {
-    await page.evaluate(async (distance) => {
-      await new Promise((resolve, reject) => {
-        var totalHeight = 0;
-        var timer = setInterval(() => {
-          var scrollHeight = document.body.scrollHeight;
-          window.scrollBy(0, distance);
-          totalHeight += distance;
+    await page.evaluate(
+      async (_step, _interval, _idlePassesToConfirm) => {
+        await new Promise((resolve, reject) => {
+          let lastHeight = 0;
+          let idlePasses = 0;
+          let timer = setInterval(() => {
+            let scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, _step);
 
-          if (totalHeight >= scrollHeight) {
-            clearInterval(timer);
-            resolve();
-          }
-        }, 200);
-      });
-    }, step);
+            if (lastHeight === scrollHeight) {
+              idlePasses++;
+            } else {
+              idlePasses = 0;
+              lastHeight = scrollHeight;
+            }
+
+            if (idlePasses >= _idlePassesToConfirm) {
+              clearInterval(timer);
+              resolve();
+            }
+          }, _interval);
+        });
+      },
+      step,
+      interval,
+      idlePassesToConfirm
+    );
   } catch (e) {
     console.log(e);
   }
