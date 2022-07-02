199105d876a63367709c019b028486173449d50d
elmaester <olegmorket@gmail.com>
2022-07-02 16:24:59 +0300

    use dependency injection to select storage type

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { cookiesPath } = require("./config");
+const { cookiesPath, puppeteerRunsHeadless } = require("./config");
 
 const puppeteer = require("puppeteer");
 const fs = require("fs");
@@ -10,12 +10,12 @@ const selectors = require("./src/selectors");
 const autoScroll = require("./src/functions/autoScroll");
 const getUniqueNames = require("./src/functions/getUniqueNames");
 const acceptCookies = require("./src/functions/acceptCookies");
-const writeNamesToFile = require("./src/functions/writeNamesToFile");
+const saveNames = require("./src/functions/saveNames");
 
 const main = async () => {
   try {
     const cookies = JSON.parse(fs.readFileSync(cookiesPath));
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: puppeteerRunsHeadless });
     const [page] = await browser.pages();
     await page.setViewport({
       width: 1920,
@@ -28,7 +28,7 @@ const main = async () => {
     await page.waitForSelector(selectors.inviters);
     await autoScroll(page);
     const uniqueNames = await getUniqueNames(page);
-    writeNamesToFile(query, uniqueNames);
+    saveNames(query, uniqueNames);
     await browser.close();
   } catch (e) {
     console.log(e);
